bf120b0668be8fb092ab69f16df5bc6b1fb8ca1b
Kyle Hollands <kylehollands@gmail.com>
2022-04-09 16:43:11 -0400

    Updated cyclistic.R file

--- cyclistic.R
+++ cyclistic.R
@@ -4,13 +4,11 @@ library(janitor)
 library(readr)
 library(tidyr)
 library(chron)
-library(dplyr, warn.conflicts = FALSE)
+library(dplyr)
 library(readr)
 library(lubridate)
-library(ggplot2)
 
-options("scipen"=10,
-dplyr.summarise.inform = FALSE)
+options("scipen"=10)
 
 # Import and combine twelve months of data into one dataset
 combined_trip_data <- list.files(path = "./Datasets/Modified CSV Files",  # Identify all CSV files
@@ -29,15 +27,11 @@ combined_trip_data <- list.files(path = "./Datasets/Modified CSV Files",  # Iden
                    ,to_station_id = end_station_id
                    ,usertype = member_casual))
 
-table(combined_trip_data$usertype)
-table(combined_trip_data$rideable_type)
-
 # Trim the dataset to only include the most relevant information
 trimmed_trip_data <- combined_trip_data %>%
   select(-c(start_lat, start_lng, end_lat, end_lng, from_station_name,from_station_id,to_station_name,to_station_id))
 
 # Inspect the data
-
 colnames(trimmed_trip_data)
 glimpse(trimmed_trip_data)
 nrow(trimmed_trip_data)
@@ -56,22 +50,17 @@ trimmed_trip_data$year <- format(as.Date(trimmed_trip_data$date), "%Y")
 # day of week as a double.)
 trimmed_trip_data$day_of_week <- format(as.Date(trimmed_trip_data$date), "%A")
 
-glimpse(trimmed_trip_data)
-
 # Determine which columns/rows contain the most NA's.
 sapply(trimmed_trip_data, function(x) sum(is.na(x)))
 
 # Remove any rows where ride length <= 0 seconds and greater than 3 hours.
 cleaned_trip_data <- trimmed_trip_data[!(trimmed_trip_data$ride_length <= 0 | trimmed_trip_data$ride_length >= 10800),]
-glimpse(cleaned_trip_data)
-sapply(cleaned_trip_data, function(x) sum(is.na(x)))
 
 # Remove rows with NAs.
 cleaned_trip_data <- na.omit(cleaned_trip_data)
-glimpse(cleaned_trip_data)
 
-sapply(cleaned_trip_data, function(x) sum(is.na(x)))
 glimpse(cleaned_trip_data)
+sapply(cleaned_trip_data, function(x) sum(is.na(x)))
 
 # Calculate initial statistics for casual riders
 
